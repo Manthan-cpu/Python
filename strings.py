d6cd1cffcab0d4e621413fbb94d423a4dd670321
Manthan Gupta <ee240002036@iiti.ac.in>
2025-07-11 12:41:44 +0000

    Pending changes exported from your codespace

---
+++ strings.py
@@ -0,0 +1,7 @@
+str1="This is a string"
+str2='this is also a string'
+str3="""this is a string"""
+str4='''this is also a string'''
+str5="This is a string with a 'single quote' inside"
+str6='This is a string with a "double quote" inside'
+str7='''This is a string with a "double quote" and a 'single quote' inside'''
